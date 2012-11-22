24dec8e81a0890aed1fe270b16a232128f4a0d90
sorenh (Dr. ) <sorenh@math.aau.dk>
2012-11-22 23:52:39 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gmwr@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,6 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
+Bla bla bla
 <p> No content added yet </p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
